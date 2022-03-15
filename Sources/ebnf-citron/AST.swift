c1cebe783e68f48a8f3f2b3bbf20cec696aa5125
Dave Abrahams <dabrahams@adobe.com>
2022-03-14 21:18:36 -0700

    Make AST a little easier to read.

--- Sources/ebnf-citron/AST.swift
+++ Sources/ebnf-citron/AST.swift
@@ -1,7 +1,30 @@
 import CitronLexerModule
 
+enum EBNF {
+  typealias RuleList = [Rule]
+  struct Rule: AST {
+    let lhs: Symbol
+    let rhs: AltList
+  }
+  typealias AltList = [Alt]
+  typealias Alt = TermList?
+  typealias TermList = [Term]
+  typealias Symbol = Token
+  
+  enum Term: AST {
+    case group(AltList)
+    case symbol(Symbol)
+    case literal(String, position: SourceRegion)
+    indirect case quantified(Term, Character, position: SourceRegion)    
+  }
+}
+
+/// An AST node.
 protocol AST {
+  /// The region of source parsed as this node.
   var position: SourceRegion { get }
+
+  /// A string representation in the original syntax.
   var dump: String { get }
 }
 
@@ -27,49 +50,32 @@ extension Optional: AST where Wrapped: AST {
   var dump: String { self?.dump ?? "" }
 }
 
-
-enum EBNF {
-  typealias RuleList = [Rule]
-  struct Rule: AST {
-    let lhs: Symbol
-    let rhs: AltList
-    var position: SourceRegion { lhs.position...rhs.position }
-    var dump: String {
-      """
-      \(position): note: rule
-      \(lhs.dump) ::=
-        \(rhs.dump)
-      """
-    }
-  }
-  typealias AltList = [Alt]
-  typealias Alt = TermList?
-  typealias TermList = [Term]
-  typealias Symbol = Token
-  
-  enum Term: AST {
-    case group(AltList)
-    case symbol(Symbol)
-    case literal(String, position: SourceRegion)
-    indirect case quantified(Term, Character, position: SourceRegion)
-    
-    var position: SourceRegion {
-      switch self {
-      case .group(let g): return g.position
-      case .symbol(let s): return s.position
-      case .literal(_, let p): return p
-      case .quantified(_, _, let p): return p
-      }
-    }
-    var dump: String {
-      switch self {
-      case .group(let g): return "( \(g.dump) )"
-      case .symbol(let s): return s.dump
-      case .literal(let s, _):
-        return "'\(s.replacingOccurrences(of: "'", with: "\\'"))'"
-      case .quantified(let t, let q, _): return t.dump + String(q)
-      }
-    }
+extension EBNF.Rule {
+  var position: SourceRegion { lhs.position...rhs.position }
+  var dump: String {
+    """
+    \(position): note: rule
+    \(lhs.dump) ::=\(rhs.count > 1 ? "\n    " : " ")\(rhs.dump)
+    """
   }
 }
 
+extension EBNF.Term {
+  var position: SourceRegion {
+    switch self {
+    case .group(let g): return g.position
+    case .symbol(let s): return s.position
+    case .literal(_, let p): return p
+    case .quantified(_, _, let p): return p
+    }
+  }
+  var dump: String {
+    switch self {
+    case .group(let g): return "( \(g.dump) )"
+    case .symbol(let s): return s.dump
+    case .literal(let s, _):
+      return "'\(s.replacingOccurrences(of: "'", with: "\\'"))'"
+    case .quantified(let t, let q, _): return t.dump + String(q)
+    }
+  }
+}
