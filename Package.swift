a44ac80270355a1e406f94c1f59c7dcda4008b59
Dave Abrahams <dabrahams@adobe.com>
2022-05-14 00:04:05 -0700

    Use build tool plugin

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/dabrahams/citron.git", branch: "scanner")
+      .package(url: "https://github.com/dabrahams/citron.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,7 +24,7 @@ let package = Package(
         .executableTarget(
             name: "ebnf-citron",
             dependencies: [CitronParser, CitronLexer],
-            exclude: ["EBNFGrammar.citron"]
+            plugins: [ .plugin(name: "CitronParserGenerator", package: "citron") ]
         ),
         .testTarget(
             name: "ebnf-citronTests",
